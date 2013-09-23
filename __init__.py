8b70a0f5328ea70cfca97f16a1ca10f6979ae84b
Maurice Ling <mauriceling@acm.org>
2013-09-23 15:14:56 +0800

    initial import

---
+++ __init__.py
@@ -0,0 +1,7 @@
+import ragaraja
+import register_machine
+
+from dose_world import World
+
+from genetic import Chromosome, Organism, Population
+from genetic import crossover, population_constructor, population_simulate
