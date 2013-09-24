6878a46325bf5f9ca69417b691463bd1fe83a102
clarencecastillo <clarence.castillo_33@yahoo.com>
2013-09-24 22:34:36 +0800

    Simulation results now saved in folders
    Merged custom_functions() and world_builder() to become Entities().

--- dose.py
+++ dose.py
@@ -1,20 +1,11 @@
 import sys, os, random
 from datetime import datetime
 
-import ragaraja
-import register_machine
-import dose_world
-import genetic
+import ragaraja, register_machine
+import dose_world, genetic
 
-class world(dose_world.World):
-    def __init__(self, world_x, world_y, world_z):
-        super(world, self).__init__(world_x, world_y, world_z)
-    def organism_movement(self, x, y, z): pass
-    def organism_location(self, x, y, z): pass
-    def ecoregulate(self): pass
-    def update_ecology(self, x, y, z): pass
-    def update_local(self, x, y, z): pass
-    def report(self): pass
+class dose_entities(dose_world.World):
+    pass
 
 def spawn_populations(p):
     temp_pop = {}
@@ -40,25 +31,25 @@ def coordinates(location):
     z = location[2]
     return (x,y,z)
 
-def deploy(p, Populations, World, population):
+def deploy(p, Populations, Entities, population):
     location = p["population_locations"][p["population_names"].index(population)]
     (x,y,z) = coordinates(location)
-    World.ecosystem[x][y][z]['organisms'] = p["population_size"]
+    Entities.ecosystem[x][y][z]['organisms'] = p["population_size"]
     for individual in Populations[population].agents:
         individual.status['location'] = location
 
-def interpret_chromosome(p, Populations, World, population, overwrite):
+def interpret_chromosome(p, Populations, Entities, population):
     for individual in Populations[population].agents:
         location = individual.status['location']
         (x,y,z) = coordinates(location)
-        inputdata = World.ecosystem[x][y][z]['local_input']
-        output = World.ecosystem[x][y][z]['local_output']
+        inputdata = Entities.ecosystem[x][y][z]['local_input']
+        output = Entities.ecosystem[x][y][z]['local_output']
         source = ''.join(individual.genome[0].sequence)
 
         if p["clean_cell"]:
             array = [0] * p["cells"]
         else:
-            array = individual.overwrite.cell
+            array = individual.Entities.cell
         
         try: (array, apointer, inputdata, output, source, spointer) = \
             register_machine.interpret(source, ragaraja.ragaraja, 3,
@@ -67,40 +58,47 @@ def interpret_chromosome(p, Populations, World, population, overwrite):
         except Exception: pass
             
         individual.cell = array
-        World.ecosystem[x][y][z]['temporary_input'] = inputdata
-        World.ecosystem[x][y][z]['temporary_output'] = output
+        Entities.ecosystem[x][y][z]['temporary_input'] = inputdata
+        Entities.ecosystem[x][y][z]['temporary_output'] = output
 
-def step(Populations, population, overwrite):
+def step(Populations, population, Entities):
     if Populations[population].generation > 0:
-        overwrite.prepopulation_control()
-    overwrite.mating()
-    overwrite.postpopulation_control()
+        Entities.prepopulation_control()
+    Entities.mating()
+    Entities.postpopulation_control()
     for organism in Populations[population].agents:
-        overwrite.mutation_scheme(organism)
-    overwrite.generation_events()
+        Entities.mutation_scheme(organism)
+    Entities.generation_events()
     Populations[population].generation += 1
-    return overwrite.population_report(Populations[population])
+    return Entities.population_report(Populations[population])
 
-def report_generation(p, Populations, population, overwrite, generation_count):
-    report = step(Populations, population, overwrite)
+def report_generation(p, Populations, population, Entities, generation_count):
+    report = step(Populations, population, Entities)
     if generation_count % int(p["fossilized_frequency"]) == 0:
-        file = '%s_%s_' % (p["simulation_code"], population)
+        file = '%s%s_%s_' % (p["directory"],
+                             p["simulation_code"], population)
         Populations[population].freeze(file, p["fossilized_ratio"])
     if generation_count % int(p["print_frequency"]) == 0:
         print '\nGENERATION: %s \n%s' % (str(generation_count), str(report))
-        f = open(('%s_%s.result.txt' % (p["simulation_code"], population)), 'a')
+        f = open(('%s%s_%s.result.txt' % (p["directory"],
+                                          p["simulation_code"], 
+                                          population)), 'a')
         dtstamp = str(datetime.utcnow())
         f.write('\n'.join(['\n' + dtstamp, 'GENERATION: ' + str(generation_count), str(report)]))
         f.write('\n')
         f.close
 
-def bury_world(p, generation_count, World):
+def bury_world(p, generation_count, Entities):
     if generation_count % int (p["eco_buried_frequency"]) == 0:
-       filename = '%s_gen%s.eco' % (p["simulation_code"], str(generation_count))
-       World.eco_burial(filename)
+       filename = '%s%s_gen%s.eco' % (p["directory"], 
+                                      p["simulation_code"], 
+                                      str(generation_count))
+       Entities.eco_burial(filename)
 
 def write_parameters(p, population):
-    f = open(('%s_%s.result.txt' % (p["simulation_code"], population)), 'a')
+    f = open(('%s%s_%s.result.txt' % (p["directory"],
+                                      p["simulation_code"], 
+                                      population)), 'a')
     f.write('''SIMULATION CODE: %(simulation_code)s                     %(starting_time)s
 ----------------------------------------------------------------------
 
@@ -135,44 +133,51 @@ REPORT:
 ''' % p)
     f.close()
 
-def simulate(parameters, world_builder, custom_functions):
-
-    overwrite = custom_functions()
+def simulate(parameters, entities):
+    
+    Entities = entities()
+    
+    time = str(datetime.utcnow())
+    directory = "%s\\Simulations\\%s_%s\\" % (os.getcwd(), parameters["simulation_code"], time[0:10])
+    
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
     parameters.update({"initial_chromosome":['0'] * parameters["chromosome_size"],
-          "mutation_scheme": overwrite.mutation_scheme,
-          "fitness_function": overwrite.fitness,
-          "prepopulation_control": overwrite.prepopulation_control,
-          "mating": overwrite.mating,
-          "postpopulation_control": overwrite.postpopulation_control,
-          "generation_events": overwrite.generation_events,
-          "population_report": overwrite.population_report,
-          "starting_time": datetime.utcnow()})
+          "mutation_scheme": Entities.mutation_scheme,
+          "fitness_function": Entities.fitness,
+          "prepopulation_control": Entities.prepopulation_control,
+          "mating": Entities.mating,
+          "postpopulation_control": Entities.postpopulation_control,
+          "generation_events": Entities.generation_events,
+          "population_report": Entities.population_report,
+          "starting_time": time,
+          "directory": directory})
 
     Populations = spawn_populations(parameters)
     World = world_builder()
-
+o
     ragaraja.activate_version(parameters["ragaraja_version"])
 
     for population in Populations:
         write_parameters(parameters, population)
-        deploy(parameters, Populations, World, population)
+        deploy(parameters, Populations, Entities, population)
     
         generation_count = 0
         while generation_count < parameters["maximum_generations"]:
             generation_count += 1
-            World.ecoregulate()
+            Entities.ecoregulate()
             
-            eco_cell_locator(parameters, World.update_ecology)
-            eco_cell_locator(parameters, World.update_local)
+            eco_cell_locator(parameters, Entities.update_ecology)
+            eco_cell_locator(parameters, Entities.update_local)
 
-            interpret_chromosome(parameters, Populations, World, population, overwrite)
+            interpret_chromosome(parameters, Populations, World, population)
 
-            report_generation(parameters, Populations, population, overwrite, generation_count)
+            report_generation(parameters, Populations, population, Entities, generation_count)
 
-            eco_cell_locator(parameters, World.organism_movement)
-            eco_cell_locator(parameters, World.organism_location)
+            eco_cell_locator(parameters, Entities.organism_movement)
+            eco_cell_locator(parameters, Entities.organism_location)
 
-            eco_cell_executor(parameters, World.report)
+            eco_cell_executor(parameters, Entities.report)
 
-            bury_world(parameters, generation_count, World)
+            bury_world(parameters, generation_count, Entities)
