bd1006fd94d3c89a17cbf94f7e2c8dd8bd0d4cf7
Clarence Castillo <clarence.castillo_33@yahoo.com>
2013-10-08 01:22:00 +0800

    Renamed 'sim_param' into 'sim_parameters' for consistency throughout dose.py

--- dose/dose.py
+++ dose/dose.py
@@ -8,10 +8,10 @@ class dose_functions(dose_world.World):
     def __init__(self, world_x, world_y, world_z):
         super(dose_functions, self).__init__(world_x, world_y, world_z)
 
-def spawn_populations(sim_param):
+def spawn_populations(sim_parameters):
     temp_Populations = {}
-    for pop_name in sim_param["population_names"]:
-        temp_Populations[pop_name] = genetic.population_constructor(sim_param)
+    for pop_name in sim_parameters["population_names"]:
+        temp_Populations[pop_name] = genetic.population_constructor(sim_parameters)
         for individual in temp_Populations[pop_name].agents:
             individual.generate_name()
             individual.status['deme'] = pop_name
@@ -65,12 +65,12 @@ def filter_status(status_key, condition, agents):
                 extract.append(individual)
     return extract
 
-def eco_cell_locator(sim_param, function):
-    for x in range(sim_param["world_x"]):
-        for y in range(sim_param["world_y"]):
-            for z in range(sim_param["world_z"]):
-                if len(inspect.getargspec(function)[0]) == 4:
-                    function(x,y,z)
+def eco_cell_iterator(World, sim_parameters, function):
+    for x in range(sim_parameters["world_x"]):
+        for y in range(sim_parameters["world_y"]):
+            for z in range(sim_parameters["world_z"]):
+                if len(inspect.getargspec(function)[0]) == 5:
+                    function(World, x, y, z)
                 else:
                     function()
 
@@ -80,10 +80,12 @@ def coordinates(location):
     z = location[2]
     return (x,y,z)
 
-def adjacent_cells(sim_param, location):
+def adjacent_cells(sim_parameters, location):
     trashbin = []
     temp_cells = []
-    world_size = [sim_param["world_x"],sim_param["world_y"],sim_param["world_z"]]
+    world_size = [sim_parameters["world_x"],
+                  sim_parameters["world_y"],
+                  sim_parameters["world_z"]]
     for i in xrange(3):
         new_location = [spot for spot in location]
         new_location[0] += 1
@@ -108,99 +110,98 @@ def adjacent_cells(sim_param, location):
         temp_cells.remove(location)
     return [tuple(location) for location in temp_cells]
     
-def deploy(sim_param, Populations, World, pop_name):
-   
-    locations = [location for location in sim_param["population_locations"][sim_param["population_names"].index(pop_name)]]
-    if sim_param["deployment_code"] == 0:
-        sim_param["deployment_scheme"](Populations, World, pop_name)
-    if sim_param["deployment_code"] == 1:
+def deploy(sim_parameters, Populations, pop_name, World):
+    position = sim_parameters["population_names"].index(pop_name)
+    locations = [location for location in sim_parameters["population_locations"][position]]
+    if sim_parameters["deployment_code"] == 0:
+        sim_parameters["deployment_scheme"](Populations, pop_name, World)
+    if sim_parameters["deployment_code"] == 1:
         location = locations[0]
         (x,y,z) = coordinates(location)
-        World.ecosystem[x][y][z]['organisms'] = sim_param["population_size"]
+        World.ecosystem[x][y][z]['organisms'] = sim_parameters["population_size"]
         for individual in Populations[pop_name].agents:
             individual.status['location'] = location
-    elif sim_param["deployment_code"] == 2:
+    elif sim_parameters["deployment_code"] == 2:
         for individual in Populations[pop_name].agents:
             location = random.choice(locations)
             (x,y,z) = coordinates(location)
-            while World.ecosystem[x][y][z]['organisms'] >= sim_param["eco_cell_capacity"]:
+            while World.ecosystem[x][y][z]['organisms'] >= sim_parameters["eco_cell_capacity"]:
                 location = random.choice(locations)
                 (x,y,z) = coordinates(location)
-            World.ecosystem[x][y][z]['organisms'] += 1
+            World.ecosystem[x][y][z]['organisms'] = World.ecosystem[x][y][z]['organisms'] + 1
             individual.status['location'] = location
-    elif sim_param["deployment_code"] == 3:
+    elif sim_parameters["deployment_code"] == 3:
         iterator = 0
-        for i in xrange(sim_param["population_size"]):
+        for i in xrange(sim_parameters["population_size"]):
             individual = Populations[pop_name].agents[i]
             location = locations[iterator]
             (x,y,z) = coordinates(location)
-            World.ecosystem[x][y][z]['organisms'] += 1
+            World.ecosystem[x][y][z]['organisms'] = World.ecosystem[x][y][z]['organisms'] + 1
             individual.status['location'] = location
             iterator += 1
             if iterator == len(locations):
                 iterator = 0
-    elif sim_param["deployment_code"] == 4:
+    elif sim_parameters["deployment_code"] == 4:
         location = locations[0]
-        adj_cells = adjacent_cells(sim_param, location)
-        for group in xrange((sim_param["population_size"]/sim_param["eco_cell_capacity"]) + 1):
-            start = sim_param["eco_cell_capacity"] * group
-            end = start + sim_param["eco_cell_capacity"]
+        adj_cells = adjacent_cells(sim_parameters, location)
+        for group in xrange((sim_parameters["population_size"]/sim_parameters["eco_cell_capacity"]) + 1):
+            start = sim_parameters["eco_cell_capacity"] * group
+            end = start + sim_parameters["eco_cell_capacity"]
             for x in xrange(start,end):
-                if x == sim_param["population_size"]: break
+                if x == sim_parameters["population_size"]: break
                 individual = Populations[pop_name].agents[x]
-                if x > (sim_param["eco_cell_capacity"] - 1):
+                if x > (sim_parameters["eco_cell_capacity"] - 1):
                     location = random.choice(adj_cells)
                     (x,y,z) = coordinates(location)
-                    while World.ecosystem[x][y][z]['organisms'] > sim_param["eco_cell_capacity"]:
+                    while World.ecosystem[x][y][z]['organisms'] > sim_parameters["eco_cell_capacity"]:
                         location = random.choice(adj_cells)
                         (x,y,z) = coordinates(location)
                 (x,y,z) = coordinates(location)
                 World.ecosystem[x][y][z]['organisms'] += 1
                 individual.status['location'] = location
 
-def interpret_chromosome(sim_param, Populations, World, pop_name):
-    cell = [0] * sim_param["cells"]
+def interpret_chromosome(sim_parameters, Populations, pop_name, World):
+    cell = [0] * sim_parameters["cells"]
     for individual in Populations[pop_name].agents:
         location = individual.status['location']
         (x,y,z) = coordinates(location)
         inputdata = World.ecosystem[x][y][z]['local_input']
         output = World.ecosystem[x][y][z]['local_output']
         source = ''.join(individual.genome[0].sequence)
-        if sim_param["clean_cell"]:
-            array = [0] * sim_param["cells"]
+        if sim_parameters["clean_cell"]:
+            array = [0] * sim_parameters["cells"]
         else:
-            array = individual.World.cell
+            array = individual.cell
         try: (array, apointer, inputdata, output, source, spointer) = \
             register_machine.interpret(source, ragaraja.ragaraja, 3,
                                        inputdata, array,
-                                       sim_param["max_cell_population"], 
-									   sim_param["max_codon"])
+                                       sim_parameters["max_cell_population"], 
+									   sim_parameters["max_codon"])
         except Exception: pass
         individual.cell = array
         World.ecosystem[x][y][z]['temporary_input'] = inputdata
         World.ecosystem[x][y][z]['temporary_output'] = output
 
-def step(Populations, pop_name, Entities):
+def step(Populations, pop_name, sim_functions):
     if Populations[pop_name].generation > 0:
-        Entities.prepopulation_control()
-    Entities.mating()
-    Entities.postpopulation_control()
+        sim_functions.prepopulation_control(Populations, pop_name)
+    sim_functions.mating(Populations, pop_name)
+    sim_functions.postpopulation_control(Populations, pop_name)
     for organism in Populations[pop_name].agents:
-        Entities.mutation_scheme(organism)
-    Entities.generation_events()
-    Populations[pop_name].generation += 1
-    return Entities.population_report(Populations, pop_name)
+        sim_functions.mutation_scheme(organism)
+    sim_functions.generation_events(Populations, pop_name)
+    Populations[pop_name].generation = Populations[pop_name].generation + 1
+    return sim_functions.population_report(Populations, pop_name)
 
-def report_generation(sim_param, Populations, pop_name, Entities, generation_count):
-    report = step(Populations, pop_name, Entities)
-    if generation_count % int(sim_param["fossilized_frequency"]) == 0:
-        file = '%s%s_%s_' % (sim_param["directory"],
-        Populations[pop_name].freeze(file, sim_param["fossilized_ratio"])
-    if generation_count % int(sim_param["print_frequency"]) == 0:
+def report_generation(sim_parameters, Populations, pop_name, sim_functions, generation_count):
+    report = step(Populations, pop_name, sim_functions)
+    if generation_count % int(sim_parameters["fossilized_frequency"]) == 0:
+        file = '%s%s_%s_' % (sim_parameters["directory"],
                              sim_parameters["simulation_name"], pop_name)
+        Populations[pop_name].freeze(file, sim_parameters["fossilized_ratio"])
+    if generation_count % int(sim_parameters["print_frequency"]) == 0:
         print '\nGENERATION: %s \n%s' % (str(generation_count), str(report))
-        f = open(('%s%s_%s.result.txt' % (sim_param["directory"],
-                                          sim_param["simulation_code"], 
+        f = open(('%s%s_%s.result.txt' % (sim_parameters["directory"],
                                           sim_parameters["simulation_name"], 
                                           pop_name)), 'a')
         dtstamp = str(datetime.utcnow())
@@ -250,8 +251,7 @@ eco_buried_frequency: %(eco_buried_frequency)s
 
 REPORT:
 ----------------------------------------------------------------------
-''' % (sim_param))
-	
+''' % (sim_parameters))
     f.close()
 
 def simulate(parameters, simulation_functions):
@@ -260,10 +260,11 @@ def simulate(parameters, simulation_functions):
                                     parameters["world_y"], 
                                     parameters["world_z"])
     time_start = str(datetime.utcnow())
-    directory = "%s\\Simulations\\%s_%s\\" % (os.getcwd(), parameters["simulation_code"], time_start[0:10])
+    directory = "%s\\Simulations\\%s_%s\\" % (os.getcwd(), 
+                                              sim_parameters["simulation_name"], 
+                                              time_start[0:10])
     if not os.path.exists(directory):
         os.makedirs(directory)
-    parameters.update({"initial_chromosome":['0'] * parameters["chromosome_size"],
 					   "fitness_function": Entities.fitness,
 					   "mutation_scheme": Entities.mutation_scheme,
 					   "prepopulation_control": Entities.prepopulation_control,
@@ -274,11 +275,10 @@ def simulate(parameters, simulation_functions):
                        "deployment_scheme": Entities.deployment_scheme,
 					   "starting_time": time_start, 
 					   "directory": directory})
-    Populations = spawn_populations(parameters)
-    ragaraja.activate_version(parameters["ragaraja_version"])
+    sim_parameters.update({"initial_chromosome":['0'] * sim_parameters["chromosome_size"],
     for pop_name in Populations:
-        write_parameters(parameters, pop_name)
-        deploy(parameters, Populations, Entities, pop_name)          
+        write_parameters(sim_parameters, pop_name)
+        deploy(sim_parameters, Populations, pop_name, World)          
         generation_count = 0
         while generation_count < parameters["maximum_generations"]:
             generation_count += 1
