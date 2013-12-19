a5505af7852b1236f14ba36c1abc418f6386db2f
Maurice HT Ling <mauriceling@acm.org>
2013-12-19 13:49:58 +0800

    reduced the description text

--- setup.py
+++ setup.py
@@ -51,46 +51,9 @@ world simulator for studying digital organisms. I will argue that digital
 organisms are considered living organisms (Koh and Ling, 2013). Despite so, 
 being a molecular biologist by training, I have a hard time mapping 
 components of digital organisms into biological life whenever such 
-components are too abstract.
-
-Hence, I decided to design an artificial life / digital organism simulator 
-that bears resemblance to biological life and ecology. These are the 
-foundation papers:
-
-    Lim, JZR, Aw, ZQ, Goh, DJW, How, JA, Low, SXZ, Loo, BZL, Ling, MHT. 
-    2010. A genetic algorithm framework grounded in biology. The Python 
-    Papers Source Codes 2: 6.
-    
-    This manuscript describes the implementation of a GA framework that 
-    uses biological hierarchy - from chromosomes to organisms to population.
-    
-    Ling, MHT. 2012. An Artificial Life Simulation Library Based on 
-    Genetic Algorithm, 3-Character Genetic Code and Biological Hierarchy. 
-    The Python Papers 7: 5. 
-    
-    Genetic algorithm (GA) is inspired by biological evolution of genetic 
-    organisms by optimizing the genotypic combinations encoded within each 
-    individual with the help of evolutionary operators, suggesting that GA 
-    may be a suitable model for studying real-life evolutionary processes. 
-    This paper describes the design of a Python library for artificial life 
-    simulation, Digital Organism Simulation Environment (DOSE), based on GA 
-    and biological hierarchy starting from genetic sequence to population. 
-    A 3-character instruction set that does not take any operand is 
-    introduced as genetic code for digital organism. This mimics the 3-
-    nucleotide codon structure in naturally occurring DNA. In addition, 
-    the context of a 3-dimensional world composing of ecological cells is 
-    introduced to simulate a physical ecosystem.
-    
-    Ling, MHT. 2012. Ragaraja 1.0: The Genome Interpreter of Digital 
-    Organism Simulation Environment (DOSE). The Python Papers Source Codes 
-    4: 2.
-    
-    This manuscript describes the implementation and test of Ragaraja 
-    instruction set version 1.0, which is the core genomic interpreter of 
-    DOSE.
-
-From this foundation, the complete suite of Digital Organisms Simulation 
-Environment (DOSE) can be build.
+components are too abstract. Hence, I decided to design an artificial 
+life / digital organism simulator that bears resemblance to biological 
+life and ecology. 
 
 License: Unless otherwise specified, all files in dose/copads folder will 
 be licensed under Python Software Foundation License version 2; all other 
@@ -118,5 +81,5 @@ Copyright 2010-2013, Maurice HT Ling (on behalf of all authors).
                    'Topic :: Scientific/Engineering :: Bio-Informatics',
                    'Topic :: Software Development :: Libraries :: Python Modules'
                    ],
-      #console=['dosecmd.py']
+      console=['dosecmd.py']
      )
