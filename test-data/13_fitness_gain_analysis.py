e459016e2af48b858691695f198ca1a8bb4ca252
Clarence Castillo <clarence.castillo_33@yahoo.com>
2014-05-03 11:36:22 +0800

    Updated fitness analyses scripts

--- test-data/13_fitness_gain_analysis.py
+++ test-data/13_fitness_gain_analysis.py
@@ -7,13 +7,12 @@ import analytics
 
 def analyze_fitness(fitness): return fitness
 
-for sequence in xrange(5, 12):
-    for trial in xrange(1, 4):
-        analysis_name = "T" + str(trial) + "_" + str(sequence) + "x0_gain_1"
-        sim13_analysis = analytics.Analysis(analysis_name + ".db", 
-                                            "pop_01", starting_time = 'default')
-        sim13_analysis.analyze_individual_status_by_generation(analysis_name + "_fitness_analysis.csv", 
-                                                               "fitness", analyze_fitness, 
-                                                               {"Average":analytics.average, 
-                                                                "STDEV":analytics.standard_deviation}, 
-                                                                xrange(1, 201))
+for trial in xrange(1, 26):
+	analysis_name = "T" + str(trial) + "_FPS_11x0_gain4"
+	sim13_analysis = analytics.Analysis(analysis_name + ".db", 
+										"pop_01", starting_time = 'default')
+	sim13_analysis.analyze_individual_status_by_generation(analysis_name + ".csv", 
+														   "fitness", analyze_fitness, 
+														   {"Average":analytics.average, 
+															"STDEV":analytics.standard_deviation}, 
+															'all')
--- test-data/14_fitness_loss_analysis.py
+++ test-data/14_fitness_loss_analysis.py
@@ -8,13 +8,12 @@ import analytics
 
 def analyze_fitness(fitness): return fitness
 
-for sequence in xrange(5, 12):
-    for trial in xrange(1, 4):
-        analysis_name = "T" + str(trial) + "_" + str(sequence) + "x0_loss_1"
-        sim14_analysis = analytics.Analysis(analysis_name + ".db", 
-                                            "pop_01", starting_time = 'default')
-        sim14_analysis.analyze_individual_status_by_generation(analysis_name + "_fitness_analysis.csv", 
-                                                               "fitness", analyze_fitness, 
-                                                               {"Average":analytics.average, 
-                                                                "STDEV":analytics.standard_deviation}, 
-                                                                xrange(201, 5201, 100))
+for trial in xrange(1, 26):
+	analysis_name = "T" + str(trial) + "_FPS_11x0_loss4"
+	sim14_analysis = analytics.Analysis(analysis_name + ".db", 
+										"pop_01", starting_time = 'default')
+	sim14_analysis.analyze_individual_status_by_generation(analysis_name + ".csv", 
+														   "fitness", analyze_fitness, 
+														   {"Average":analytics.average, 
+															"STDEV":analytics.standard_deviation}, 
+															range(15801, 20801, 100) + [20800])
