3a6746da68161af704cf5ec894f6b03a91aa4f3a
Maurice HT Ling <mauriceling@acm.org>
2017-09-27 13:59:39 +0800

    # changed from absolute import (python 2) to relative import (python 3)

--- dose/__init__.py
+++ dose/__init__.py
@@ -3,7 +3,7 @@ Digital Organism Simulation Environment (DOSE)
 
 Date created: 27th September 2013
 '''
-import metadata
+from . import metadata
 
 __version__ = metadata.version
 __maintainer__ = metadata.maintainer
@@ -11,45 +11,45 @@ __email__ = metadata.email
 __description__ = metadata.long_description
 
 # Package imports (in ascending order of package names)
-import copads
+from . import copads
 
 # Module imports (in ascending order of module names)
-import database_calls
-import dose
-import genetic
-import register_machine
-import ragaraja
+from . import database_calls
+from . import dose
+from . import genetic
+from . import register_machine
+from . import ragaraja
 
 # COPADS Class imports (in ascending order of module names, then class names)
-from copads.lindenmayer import lindenmayer
+from .copads.lindenmayer import lindenmayer
 
 # DOSE Class imports (in ascending order of module names, then class names)
-from dose import dose_functions
-from dose_world import World
-from genetic import Chromosome
-from genetic import Organism
-from genetic import Population 
+from .dose import dose_functions
+from .dose_world import World
+from .genetic import Chromosome
+from .genetic import Organism
+from .genetic import Population 
 
 # Function imports (in ascending order of module names, then function names)
-from database_calls import connect_database
-from database_calls import db_list_datafields
-from database_calls import db_list_generations
-from database_calls import db_list_simulations
-from database_calls import db_list_population_name
-from database_calls import db_get_ecosystem
-from database_calls import db_get_organisms_chromosome_sequences
-from database_calls import db_get_organisms_genome
-from database_calls import db_get_organisms_status
-from dose import database_report_populations
-from dose import database_report_world
-from dose import filter_age
-from dose import filter_deme
-from dose import filter_gender
-from dose import filter_location
-from dose import filter_status
-from dose import filter_vitality
-from dose import revive_simulation
-from dose import simulate
-from genetic import crossover
-from genetic import population_constructor
-from genetic import population_simulate
+from .database_calls import connect_database
+from .database_calls import db_list_datafields
+from .database_calls import db_list_generations
+from .database_calls import db_list_simulations
+from .database_calls import db_list_population_name
+from .database_calls import db_get_ecosystem
+from .database_calls import db_get_organisms_chromosome_sequences
+from .database_calls import db_get_organisms_genome
+from .database_calls import db_get_organisms_status
+from .dose import database_report_populations
+from .dose import database_report_world
+from .dose import filter_age
+from .dose import filter_deme
+from .dose import filter_gender
+from .dose import filter_location
+from .dose import filter_status
+from .dose import filter_vitality
+from .dose import revive_simulation
+from .dose import simulate
+from .genetic import crossover
+from .genetic import population_constructor
+from .genetic import population_simulate
