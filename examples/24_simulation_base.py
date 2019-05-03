94b13636805aa5b95130858cb73d7615ad60c766
Maurice HT Ling <mauriceling@acm.org>
2019-05-03 21:10:36 +0800

    + added prepopulation_control step

--- examples/24_simulation_base.py
+++ examples/24_simulation_base.py
@@ -6,10 +6,15 @@ In this simulation,
     - 1 population of 100 organisms
     - each organism will have 1 chromosome of only 4 bases (A, T, G, C)
     - entire population will be deployed in one eco-cell (0, 0, 0)
-    - 10% background point mutation on chromosome of 30 bases
+    - 10% background point mutation on chromosome of 100 bases
     - no organism movement throughout the simulation
-    - fitness is calculated as minimum pairwise alignment of organism 
+    - fitness is calculated as average pairwise alignment of organism 
     chromosome to known sequences
+    - the lowest dectile of the organisms (by fitness) will be removed 
+    if there are more than 50% population remaining after removal; or 
+    else, a random selection of 10 organisms will be removed.
+    - a random selection of organisms after removal will be replicated 
+    to top up the population to 100 organisms
     - 100 generations to be simulated
 '''
 # needed to run this example without prior
@@ -28,6 +33,8 @@ from Bio import Align
 aligner = Align.PairwiseAligner()
 aligner.mode = str('global')
 
+fitness_threshold = 25
+
 known_sequences = ["ATAGCAGTAGCTAGTCGATGCTAGCTAG"]
 
 parameters = {# Part 1: Simulation metadata
@@ -39,7 +46,7 @@ parameters = {# Part 1: Simulation metadata
               "world_y": 1,
               "world_z": 1,
               "population_locations": [[(0,0,0)]],
-              "eco_cell_capacity": 100,
+              "eco_cell_capacity": 1000,
               "deployment_code": 1,
 
               # Part 3: Population settings
@@ -95,27 +102,50 @@ class simulation_functions(dose.dose_functions):
     def report(self, World): pass
 
     def fitness(self, Populations, pop_name):
-      for index in range(len(Populations[pop_name].agents)):
-        organism = Populations[pop_name].agents[index]
-        chromosome = ''.join(organism.genome[0].sequence)
-        score = min([aligner.score(chromosome, seq) 
-                     for seq in known_sequences])
-        Populations[pop_name].agents[index].status['fitness'] = score
+        for index in range(len(Populations[pop_name].agents)):
+            organism = Populations[pop_name].agents[index]
+            chromosome = ''.join(organism.genome[0].sequence)
+            score = [aligner.score(chromosome, seq) 
+                     for seq in known_sequences]
+            score = sum(score) / len(score)
+            Populations[pop_name].agents[index].status['fitness'] = score
 
     def mutation_scheme(self, organism): 
         organism.genome[0].rmutate(parameters["mutation_type"],
                                    parameters["additional_mutation"])
 
-    def prepopulation_control(self, Populations, pop_name): pass
+    def prepopulation_control(self, Populations, pop_name): 
+        agentslist = Populations[pop_name].agents
+        fitnesslist = [(index, agentslist[index].status['fitness'])
+            for index in range(len(agentslist))]
+        eliminationlist = [x[1] for x in fitnesslist]
+        eliminationlist.sort()
+        if len([x for x in eliminationlist if x > eliminationlist[9]]) > 50:
+            elimination_threshold = eliminationlist[9]
+            Populations[pop_name].agents = \
+                [Populations[pop_name].agents[i] 
+                 for i in range(len(Populations[pop_name].agents))
+                    if Populations[pop_name].agents[i].status['fitness'] > elimination_threshold]
+        else:
+            eliminationlist = [x[0] for x in fitnesslist]
+            eliminationlist = [random.choice(eliminationlist) 
+                               for x in range(10)]
+            Populations[pop_name].agents = \
+                [Populations[pop_name].agents[i] 
+                 for i in range(len(Populations[pop_name].agents))
+                    if i not in eliminationlist]
+        print("population size: " + str(len(Populations[pop_name].agents)))
 
     def mating(self, Populations, pop_name):
-      fitness = [org.status['fitness'] for org in Populations[pop_name].agents] 
-      index = fitness.index(max(fitness))
-      elimination = [x for x in range(parameters["population_size"]) 
-                     if x != index]
-      elimination = random.choice(elimination)
-      replicated = copy.deepcopy(Populations[pop_name].agents[index])
-      Populations[pop_name].agents[elimination] = replicated
+        agentslist = Populations[pop_name].agents
+        if len(agentslist) <= 100:
+            replicationlist = [index for index in range(len(agentslist))
+                if agentslist[index].status['fitness'] > fitness_threshold]
+            replicationlist = [random.choice(replicationlist) 
+                for x in range(100-len(agentslist))]
+            for index in replicationlist:
+                new_agent = copy.deepcopy(agentslist[index])
+                Populations[pop_name].agents.append(new_agent)
 
     def postpopulation_control(self, Populations, pop_name): pass
 
@@ -127,8 +157,8 @@ class simulation_functions(dose.dose_functions):
         locations = [str(org.status['location']) for org in Populations[pop_name].agents]
         demes = [org.status['deme'] for org in Populations[pop_name].agents]
         fitness = [org.status['fitness'] for org in Populations[pop_name].agents]
-        print(fitness)
-        print(max(fitness), sum(fitness)/len(fitness))
+        gen_count = Populations[pop_name].agents[0].status["generation"]
+        print(gen_count, max(fitness), sum(fitness)/len(fitness))
         return '\n'.join(sequences)
 
     def database_report(self, con, cur, start_time, 
