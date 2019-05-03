9ecb386c8b1f4fedf78cb99d2a257b6aceb3f9ea
Maurice HT Ling <mauriceling@acm.org>
2019-05-03 21:29:27 +0800

    # reducing number of words

--- examples/24_simulation_base.py
+++ examples/24_simulation_base.py
@@ -102,74 +102,73 @@ class simulation_functions(dose.dose_functions):
     def report(self, World): pass
 
     def fitness(self, Populations, pop_name):
-        for index in range(len(Populations[pop_name].agents)):
-            organism = Populations[pop_name].agents[index]
+        agents = Populations[pop_name].agents
+        for index in range(len(agents)):
+            organism = agents[index]
             chromosome = ''.join(organism.genome[0].sequence)
             score = [aligner.score(chromosome, seq) 
                      for seq in known_sequences]
             score = sum(score) / len(score)
-            Populations[pop_name].agents[index].status['fitness'] = score
+            agents[index].status['fitness'] = score
 
     def mutation_scheme(self, organism): 
         organism.genome[0].rmutate(parameters["mutation_type"],
                                    parameters["additional_mutation"])
 
     def prepopulation_control(self, Populations, pop_name): 
-        agentslist = Populations[pop_name].agents
-        fitnesslist = [(index, agentslist[index].status['fitness'])
-            for index in range(len(agentslist))]
-        eliminationlist = [x[1] for x in fitnesslist]
-        eliminationlist.sort()
-        if len([x for x in eliminationlist if x > eliminationlist[9]]) > 50:
-            elimination_threshold = eliminationlist[9]
+        agents = Populations[pop_name].agents
+        fitness = [(index, agents[index].status['fitness'])
+                   for index in range(len(agents))]
+        eliminate = [x[1] for x in fitness]
+        eliminate.sort()
+        if len([x for x in eliminate if x > eliminate[9]]) > 50:
+            ethreshold = eliminate[9]
             Populations[pop_name].agents = \
-                [Populations[pop_name].agents[i] 
-                 for i in range(len(Populations[pop_name].agents))
-                    if Populations[pop_name].agents[i].status['fitness'] > elimination_threshold]
+                [agents[i] for i in range(len(agents))
+                    if agents[i].status['fitness'] > ethreshold]
         else:
-            eliminationlist = [x[0] for x in fitnesslist]
-            eliminationlist = [random.choice(eliminationlist) 
-                               for x in range(10)]
+            eliminate = [x[0] for x in fitness]
+            eliminate = [random.choice(eliminate) for x in range(10)]
             Populations[pop_name].agents = \
-                [Populations[pop_name].agents[i] 
-                 for i in range(len(Populations[pop_name].agents))
-                    if i not in eliminationlist]
-        print("population size: " + str(len(Populations[pop_name].agents)))
+                [agents[i] for i in range(len(agents))
+                    if i not in eliminate]
+        print("Population size after elimination: " + \
+            str(len(Populations[pop_name].agents)))
 
     def mating(self, Populations, pop_name):
-        agentslist = Populations[pop_name].agents
-        if len(agentslist) <= 100:
-            replicationlist = [index for index in range(len(agentslist))
-                if agentslist[index].status['fitness'] > fitness_threshold]
-            replicationlist = [random.choice(replicationlist) 
-                for x in range(100-len(agentslist))]
-            for index in replicationlist:
-                new_agent = copy.deepcopy(agentslist[index])
-                Populations[pop_name].agents.append(new_agent)
+        agents = Populations[pop_name].agents
+        if len(agents) <= 100:
+            replicate = \
+                [index for index in range(len(agents))
+                 if agents[index].status['fitness'] > fitness_threshold]
+            replicate = [random.choice(replicate) 
+                         for x in range(100-len(agents))]
+            for index in replicate:
+                new_agent = copy.deepcopy(agents[index])
+                agents.append(new_agent)
 
     def postpopulation_control(self, Populations, pop_name): pass
 
     def generation_events(self, Populations, pop_name): pass
 
     def population_report(self, Populations, pop_name):
-        sequences = [''.join(org.genome[0].sequence) for org in Populations[pop_name].agents]
-        identities = [org.status['identity'] for org in Populations[pop_name].agents]
-        locations = [str(org.status['location']) for org in Populations[pop_name].agents]
-        demes = [org.status['deme'] for org in Populations[pop_name].agents]
-        fitness = [org.status['fitness'] for org in Populations[pop_name].agents]
-        gen_count = Populations[pop_name].agents[0].status["generation"]
+        agents = Populations[pop_name].agents
+        sequences = [''.join(org.genome[0].sequence) for org in agents]
+        identities = [org.status['identity'] for org in agents]
+        locations = [str(org.status['location']) for org in agents]
+        demes = [org.status['deme'] for org in agents]
+        fitness = [org.status['fitness'] for org in agents]
+        gen_count = agents[0].status["generation"]
         print(gen_count, max(fitness), sum(fitness)/len(fitness))
         return '\n'.join(sequences)
 
     def database_report(self, con, cur, start_time, 
                         Populations, World, generation_count):
-        try:
-            dose.database_report_populations(con, cur, start_time, 
-                                             Populations, generation_count)
+        try: dose.database_report_populations(con, cur, start_time, 
+                                    Populations, generation_count)
         except: pass
-        try:
-            dose.database_report_world(con, cur, start_time, 
-                                       World, generation_count)
+        try: dose.database_report_world(con, cur, start_time, 
+                                        World, generation_count)
         except: pass
 
     def deployment_scheme(self, Populations, pop_name, World): pass
