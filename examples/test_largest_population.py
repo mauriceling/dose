34ee2561a8e49ecc66cb2cd7849a5a94c8c51c14
Maurice Ling <mauriceling@acm.org>
2021-04-18 10:38:57 +0800

    + script to test the largest number of organisms DOSE can handle - about 36,000 organisms of 10k bases per organism

---
+++ examples/test_largest_population.py
@@ -0,0 +1,180 @@
+'''
+This example is to find out the largest population (largest number of 
+organisms that can be created) before hitting memory limit.
+
+In this simulation,
+    - construct world
+    - construct 1 population of incremental number of organisms (from 
+    100 and increment by 100)
+    - each organism has 1 chromosome of 10 thousand bases as genome
+'''
+# needed to run this example without prior
+# installation of DOSE into Python site-packages
+try: 
+	import run_examples_without_installation
+except ImportError: pass
+
+# Example codes starts from here
+from pprint import pprint
+import random
+import dose
+
+
+parameters = {# Part 1: Simulation metadata
+              "simulation_name": "31_construct_simulation",
+              "population_names": ['pop_01'],
+
+              # Part 2: World settings
+              "world_x": 1,
+              "world_y": 1,
+              "world_z": 1,
+              "population_locations": None,
+              "eco_cell_capacity": 100,
+              "deployment_code": None,
+
+              # Part 3: Population settings
+              "population_size": 10,
+
+              # Part 4: Genetics settings
+              "genome_size": 1,
+              "chromosome_size": 100,
+              "chromosome_bases": ['A', 'T', 'G', 'C'],
+              "initial_chromosome": ['A', 'T', 'G', 'C'] * 25,
+
+              # Part 5: Mutation settings
+              "background_mutation": 0.05,
+              "additional_mutation": 0,
+              "mutation_type": 'point',
+              
+              # Part 6: Metabolic settings
+              "interpreter": dose.codonA.interpreter,
+              "instruction_size": 3,
+              "ragaraja_version": "user-defined",
+              "base_converter": None,
+              "ragaraja_instructions": [],
+              "max_tape_length": 4,
+              "interpret_chromosome": False,
+              "clean_cell": True,
+              "max_codon": 2000,
+
+              # Part 7: Simulation settings
+              "goal": 0,
+              "maximum_generations": 100,
+              "eco_buried_frequency": 100,
+              "fossilized_ratio": 0.01,
+              "fossilized_frequency": 20,
+              
+              # Part 8: Simulation report settings
+              "print_frequency": 10,
+              "database_file": "simulation.db",
+              "database_logging_frequency": 1
+             }
+
+class simulation_functions(dose.dose_functions):
+
+    def organism_movement(self, Populations, pop_name, World): pass
+
+    def organism_location(self, Populations, pop_name, World): pass
+
+    def ecoregulate(self, World): pass
+
+    def update_ecology(self, World, x, y, z): pass
+
+    def update_local(self, World, x, y, z): pass
+
+    def report(self, World): pass
+
+    def fitness(self, Populations, pop_name): pass
+
+    def mutation_scheme(self, organism): 
+        organism.genome[0].rmutate(parameters["mutation_type"],
+                                   parameters["additional_mutation"])
+
+    def prepopulation_control(self, Populations, pop_name): pass
+
+    def mating(self, Populations, pop_name): pass
+
+    def postpopulation_control(self, Populations, pop_name): pass
+
+    def generation_events(self, Populations, pop_name): pass
+
+    def population_report(self, Populations, pop_name):
+        for org in Populations[pop_name].agents:
+          generation = str(org.status["generation"])
+          deme = org.status['deme']
+          identity = str(org.status['identity'])
+          sequence = str(''.join(org.genome[0].sequence))
+          print("|".join([generation, deme, identity, sequence]))
+
+    def database_report(self, con, cur, start_time, 
+                        Populations, World, generation_count):
+        try:
+            dose.database_report_populations(con, cur, start_time, 
+                                             Populations, generation_count)
+        except: pass
+        try:
+            dose.database_report_world(con, cur, start_time, 
+                                       World, generation_count)
+        except: pass
+
+    def deployment_scheme(self, Populations, pop_name, World): pass
+
+print('\n[' + parameters["simulation_name"].upper() + ' SIMULATION]')
+print('Adding deployment scheme to simulation parameters...')
+parameters["deployment_scheme"] = simulation_functions.deployment_scheme
+
+# Step 1: Construct World
+print('Constructing World entity...')
+# World(parameters["world_x"], parameters["world_y"], parameters["world_z"])
+World = dose.dose_world.World(1, 1, 1)
+World = dose.load_all_local_input(World, [1, 2])
+
+# Step 2: Construct Population(s)
+def construct_PopDiffOrg(World, parameters, population_dictionary):
+  organisms = []
+  for org_key in population_dictionary:
+    orgData = population_dictionary[org_key]
+    chromosomes = [dose.genetic.Chromosome(chr_seq,
+                    orgData["bases"], orgData["background_mutation"])
+                    for chr_seq in orgData["genome"]]
+    organism = dose.genetic.Organism(chromosomes,
+                                      orgData['mutation_type'],
+                                      orgData['additional_mutation'])
+    organism.status = orgData["status"]
+    x = orgData["status"]["location"][0]
+    y = orgData["status"]["location"][1]
+    z = orgData["status"]["location"][2]
+    World.ecosystem[x][y][z]["organisms"] = World.ecosystem[x][y][z]["organisms"] + 1
+    organisms.append(organism)
+  population = dose.genetic.Population(parameters["goal"],
+                                       parameters["maximum_generations"],
+                                       organisms)
+  return (World, population)
+
+import time
+
+pop_size = 100
+while True:
+  start_time = time.time()
+  pop_01 = {}
+  for i in range(0, pop_size):
+    identity = "O" + str(i)
+    genome = [["A", "T", "G", "C"] * 2500]
+    pop_01[identity] = {"status": {"alive": True, "vitality": 100.0, 
+                                   "parents": None, "age": 0.0, 
+                                   "gender": None, "lifespan": 100.0, 
+                                   "fitness": 0.0, "blood": None, 
+                                   "identity": identity, "deme": "pop_01", 
+                                   "location": (0,0,0), "generation": 0, 
+                                   "death": None},
+                       "genome": genome,
+                       "bases": parameters["chromosome_bases"],
+                       "background_mutation": parameters["background_mutation"],
+                       "additional_mutation": parameters["additional_mutation"],
+                       "mutation_type": parameters["mutation_type"]}
+
+  (World, population_01) = construct_PopDiffOrg(World, parameters, pop_01)
+  Populations = {'pop_01': population_01}
+  end_time = time.time()
+  print(pop_size, end_time-start_time)
+  pop_size = pop_size + 100
