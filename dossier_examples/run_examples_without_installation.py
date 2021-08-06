371a6aa9bc754d426edc698105e958e800142d95
Maurice Ling <mauriceling@acm.org>
2021-08-06 12:01:55 +0800

    # moved to dossier_examples folder

---
+++ dossier_examples/run_examples_without_installation.py
@@ -0,0 +1,16 @@
+'''
+This file contains inserts the path for DOSE into system path to
+allow for any examples importing this file to execute without 
+needing DOSE to be installed into into Python site-packages.
+'''
+import sys
+import os
+
+cwd = os.getcwd().split(os.sep)
+#cwd[-1] = 'dose'
+cwd = os.sep.join(cwd[:-1])
+
+if cwd not in sys.path:
+    sys.path.append(cwd)
+
+import dose
