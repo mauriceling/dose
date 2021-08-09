1edee07fd1651efb2da586de9e39fdd8c3cf52f3
iamargho <argho.official@gmail.com>
2021-08-09 19:33:48 +0530

    + compute undirected density of an organism

---
+++ dossier_examples/dossier_example_10_organism_density_undirected.py
@@ -0,0 +1,38 @@
+"""!
+Example 10 of DOSSIER [Functions to Analyze Simulation Results 
+Database and Periodic Archives from DOSE (Digital Organism Simulation 
+Environment) Simulations] - Process Density (Undirected) Scores. 
+
+Date created: 9th August 2021
+"""
+try: 
+    import run_examples_without_installation
+except ImportError: pass
+
+import dossier
+import sunanda_metabolism as metab
+
+print("OPERATION: Connect to a DOSE simulation results database")
+db = dossier.ConnectDB("..//Simulations//control_simulation_high_mutation.db")
+print("")
+
+print("OPERATION: List available simulation results")
+dataframe = db.Sims()
+print("SQL statement used: %s" % db.last_sql_statement)
+start_time = dataframe.iloc[0]['start_time']
+print(dataframe)
+print("")
+
+simSet = {}
+for index, row in dataframe.iterrows():
+    simSet[row["start_time"]] = index
+print("simSet = " + str(simSet))
+
+fitnessFunction = dossier.DensityUndirected
+fitnessDF = dossier.GenerateFitness(fitnessFunction, simSet, db, 
+                                    enzymatic_genes=metab.enzymatic_genes)
+print("Fitness Score Data Frame ..............................")
+print(fitnessDF)
+
+dossier.SaveDataframe(fitnessDF, "dossier_example_10.csv", "csv")
+dossier.SaveDataframe(fitnessDF, "dossier_example_10.xlsx", "xlsx")
