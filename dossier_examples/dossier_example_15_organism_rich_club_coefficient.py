b64762ac621ee4cad7f1a1bc1d75230b4017c05d
arghomaitra <arghomaitra@outlook.com>
2022-05-07 10:01:13 +0530

    Add file for rich club coefficient

---
+++ dossier_examples/dossier_example_15_organism_rich_club_coefficient.py
@@ -0,0 +1,39 @@
+"""!
+Example 15 of DOSSIER [Functions to Analyze Simulation Results 
+Database and Periodic Archives from DOSE (Digital Organism Simulation 
+Environment) Simulations] - Process Rich Club Coefficient Scores. 
+
+Date created: 4th January 2022
+"""
+try: 
+    import run_examples_without_installation
+except ImportError: pass
+
+import dossier
+import sunanda_metabolism as metab
+
+print("OPERATION: Connect to a DOSE simulation results database")
+db = dossier.ConnectDB("..//Simulations//simulation.db")
+print("")
+
+print("OPERATION: List available simulation results")
+dataframe = db.Sims()
+print("SQL statement used: %s" % db.last_sql_statement)
+start_time = dataframe.iloc[0]['start_time']
+print(dataframe)
+print("")
+
+simSet = {}
+for index, row in dataframe.iterrows():
+    simSet[row["start_time"]] = index
+print("simSet = " + str(simSet))
+
+fitnessFunction = dossier.MeasureRichClubCoefficient
+fitnessDF = dossier.GenerateFitness(fitnessFunction, simSet, db, 
+                                    measure=metab.enzymatic_reactions,
+                                    len_of_measure=2)
+print("Fitness Score Data Frame ..............................")
+print(fitnessDF)
+
+dossier.SaveDataframe(fitnessDF, "dossier_example_15.csv", "csv")
+dossier.SaveDataframe(fitnessDF, "dossier_example_15.xlsx", "xlsx")
